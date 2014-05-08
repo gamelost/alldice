f0252a6bb71b7fa20d08c3ef9f6a77747eaa7e5e
Anja Berens <pharaun666@gmail.com>
2014-05-08 02:09:43 -0700

    Made a shitty little http server, need to fix lots of things up

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,77 @@
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, TypeFamilies #-}
+module Main where
 
-main = undefined
+import Network.Wai
+import Network.Wai.Middleware.Routes
+import Network.Wai.Middleware.Routes.ContentTypes
+import Network.Wai.Application.Static
+import Network.Wai.Handler.Warp
+import Network.HTTP.Types
+import qualified Data.Text as T
+import Data.Text (Text)
+import Data.Aeson hiding (json)
+import Data.IORef
+import qualified Data.Map as M
+import Control.Monad.Trans
+import Network.Wai.Middleware.RequestLogger
+
+import System.Random.MWC
+
+-- The Site argument
+type Rand = GenIO
+data MyRoute = MyRoute (IORef Rand)
+
+-- Make MyRoute Routable
+mkRoute "MyRoute" [parseRoutes|
+/             HomeR           GET
+/roll         RollR           GET
+|]
+
+
+-- Handlers
+
+-- Util: Fetch the database
+getDB :: HandlerM MyRoute Rand
+getDB = do
+  MyRoute dbref <- master
+  liftIO $ readIORef dbref
+
+-- Display the possible actions
+getHomeR :: Handler MyRoute
+getHomeR = runHandlerM $ do
+  json $ M.fromList (
+                 [("description", [["Simple Dice Roller Example"]])
+                 ,("links"
+                  ,[["home", showRoute HomeR]
+                   ,["roll", showRoute RollR]
+                   ]
+                  )
+                 ] :: [(Text, [[Text]])] )
+
+
+-- Perform a simple roll
+getRollR :: Handler MyRoute
+getRollR = runHandlerM $ do
+  db <- getDB
+  -- TODO: roll some kicking rad dices
+
+  json $ M.fromList (
+          [("description", [["Dice Rolls"]])
+          ,("roll", [["6 - chosen by fair dice"]])] :: [(Text, [[Text]])] )
+
+
+-- The application that uses our route
+-- NOTE: We use the Route Monad to simplify routing
+application :: RouteM ()
+application = do
+    gen <- liftIO $ createSystemRandom
+    db <- liftIO $ newIORef gen
+    middleware logStdoutDev
+    route (MyRoute db)
+    defaultAction $ staticApp $ defaultFileServerSettings "static"
+
+-- Run the application
+main :: IO ()
+main = do
+  putStrLn "Starting server on port 8080"
+  toWaiApp application >>= run 8080
