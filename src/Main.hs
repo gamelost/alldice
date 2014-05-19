4c06606c05b6469b12dcfe9e649978d62fd641d3
Anja Berens <pharaun666@gmail.com>
2014-05-18 19:21:31 -0700

    Migrated the scheme repl into the http service, still need to do the parm parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,36 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, TypeFamilies, FlexibleContexts, MultiParamTypeClasses #-}
 module Main where
 
+import Control.Applicative
+import Control.Monad
+import Control.Monad.ST
+import Control.Monad.Trans
+
+import Data.Aeson hiding (json, String)
+import Data.IORef
+import Data.STRef
+import Data.Text (Text)
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+import Network.HTTP.Types
 import Network.Wai
-import Network.Wai.Middleware.Routes
-import Network.Wai.Middleware.Routes.ContentTypes
 import Network.Wai.Application.Static
 import Network.Wai.Handler.Warp
-import Network.HTTP.Types
-import qualified Data.Text as T
-import Data.Text (Text)
-import Data.Aeson hiding (json)
-import Data.IORef
-import qualified Data.Map as M
-import Control.Monad.Trans
 import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.Routes
+import Network.Wai.Middleware.Routes.ContentTypes
 
+import System.Environment
+import System.IO
 import System.Random.MWC
+
+import Scheme.Types
+import Scheme.Env
+import Scheme.Parser
+import Scheme.Evaluator
+import Scheme.Primitives
 
 -- The Site argument
 type Rand = GenIO
@@ -40,24 +55,55 @@ getDB = do
 getHomeR :: Handler MyRoute
 getHomeR = runHandlerM $ do
   json $ M.fromList (
-                 [("description", [["Simple Dice Roller Example"]])
-                 ,("links"
-                  ,[["home", showRoute HomeR]
-                   ,["roll", showRoute RollR]
-                   ]
-                  )
-                 ] :: [(Text, [[Text]])] )
-
+                 [ ("description", "URI endpoint")
+                 , ("home", showRoute HomeR)
+                 , ("roll", showRoute RollR)
+                 ] :: [(Text, Text)])
 
 -- Perform a simple roll
 getRollR :: Handler MyRoute
 getRollR = runHandlerM $ do
   db <- getDB
-  -- TODO: roll some kicking rad dices
+
+  -- Run the scheme interpreter here in runST then return the result
+  let roll = runST $ runExpr "(+ 1 2)"
 
   json $ M.fromList (
-          [("description", [["Dice Rolls"]])
-          ,("roll", [["6 - chosen by fair dice"]])] :: [(Text, [[Text]])] )
+          [ ("description", "Scheme Dice Roll")
+          , ("input", "(+ 1 2)")
+          , ("error", "")
+          , ("output", roll)
+          ] :: [(Text, Text)])
+
+
+evalString :: LispEnv s -> T.Text -> ST s T.Text
+evalString env expr = do
+    exp <- evalExpr env expr
+    case exp of
+        Left err  -> return $ (T.pack . show) err
+        Right val -> return $ (T.pack . show) val
+
+evalExpr :: LispEnv s -> T.Text -> ST s (ThrowsError (LispVal s))
+evalExpr env expr =
+    case readExpr expr of
+        Left err  -> return $ Left err
+        Right val -> eval env val
+
+runExpr :: T.Text -> ST s T.Text
+runExpr val = do
+    env <- primitiveBindings
+    evalString env val
+
+-----------------------------------------
+--runOne :: [T.Text] -> IO ()
+--runOne args = do
+--    file <- (T.readFile $ T.unpack (head args)) >>= return . readExprList
+--    case file of
+--        Left err  -> T.hPutStrLn stderr (T.pack $ show err)
+--        Right val -> do
+--            env <- stToIO primitiveBindings >>= \env -> stToIO (bindVars env [("args", List $ map String $ drop 1 args)])
+--            stToIO (mapM (eval env) val) >>= T.hPutStrLn stderr . T.pack . show
+-----------------------------------------
 
 
 -- The application that uses our route
@@ -73,5 +119,5 @@ application = do
 -- Run the application
 main :: IO ()
 main = do
-  putStrLn "Starting server on port 8080"
-  toWaiApp application >>= run 8080
+    putStrLn "Starting server on port 8080"
+    toWaiApp application >>= run 8080
