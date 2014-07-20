05b59665c01a300641c45c03efc919699c678630
Anja Berens <pharaun666@gmail.com>
2014-07-20 02:19:19 -0700

    theorical nsq bindings, still need to graft the interpreter logic into the nsq bindings

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,13 @@
 module Main where
 
 import Control.Applicative
+import Control.Concurrent (killThread)
+import Control.Concurrent.Async
+import Control.Concurrent.STM
 import Control.Monad
 import Control.Monad.ST
 import Control.Monad.Trans
+import qualified Control.Exception as E
 
 import Data.Aeson hiding (json, String, Number)
 import Data.IORef
@@ -27,17 +31,29 @@ import Network.Wai.Middleware.Routes.ContentTypes
 import System.Environment
 import System.IO
 
+-- Ekg monitoring
+import System.Remote.Monitoring
+
+-- NSQ endpoint
+import qualified Network.NSQ as NSQ
+
+-- Logger
+import System.IO (stderr, Handle)
+import System.Log.Logger (rootLoggerName, setHandlers, updateGlobalLogger, Priority(DEBUG), setLevel, infoM)
+import System.Log.Handler.Simple (streamHandler, GenericHandler)
+import System.Log.Handler (setFormatter)
+import System.Log.Formatter
+
 -- Not ideal but should in theory work for now
 import System.Random
 
--- Ekg monitoring
-import System.Remote.Monitoring
 
 import Scheme.Types
 import Scheme.Env
 import Scheme.Parser
 import Scheme.Evaluator
 import Scheme.Primitives
+
 
 -- The Site argument
 type SchemeRandom = StdGen
@@ -162,14 +178,83 @@ application path = do
     route (MyRoute db path)
     defaultAction $ staticApp $ defaultFileServerSettings "static"
 
+
+-- Perform a single roll
+consumeMessages :: T.Text -> TQueue NSQ.Message -> TQueue NSQ.Command -> IO ()
+consumeMessages path q r = forever $ do
+    msg <- atomically (do
+        m <- readTQueue q
+        -- Process data here
+
+        -- TODO: Unsafe, assumes it only get Messages (true as of current implementation, but still unsafe)
+        writeTQueue r $ NSQ.Fin $ mId m
+        return m)
+    infoM "Client.Consume" (show msg)
+
+    where
+        mId (NSQ.Message _ _ mesgId _) = mesgId
+--            -- Run the scheme interpreter here in runST & runRand then return the result
+--            gen <- liftIO newStdGen
+--
+--            -- Load the stdlib
+--            stdlib <- getFile
+--  MyRoute _ path <- master
+--  liftIO $ T.readFile (T.unpack path)
+--
+--            let roll = runST $ runExpr stdlib val gen
+--
+--            -- TODO: debug
+----            liftIO $ putStrLn $ T.unpack roll
+--
+--            json $ M.fromList (
+--                  [ ("description", "Scheme Dice Roll")
+--                  , ("input", val)
+--                  , ("error", "")
+--                  , ("output", roll)
+--                  ] :: [(Text, Text)])
+
+
+-- Log Formatter
+withFormatter :: GenericHandler System.IO.Handle -> GenericHandler System.IO.Handle
+withFormatter handler = setFormatter handler formatter
+    where formatter = simpleLogFormatter "[$time $loggername $prio] $msg"
+
 -- Run the application
 main :: IO ()
 main = do
-    putStrLn "Starting ekg monitoring on port 8081"
---    forkServer "localhost" 8081
+    -- Logger stuff
+    stream <- withFormatter <$> streamHandler stderr DEBUG
+    let loggerName = rootLoggerName
 
-    putStrLn "Starting server on port 8080"
-    toWaiApp (application "src/stdlib.scm") >>= run 8080
+    updateGlobalLogger loggerName (setLevel DEBUG)
+    updateGlobalLogger loggerName (setHandlers [stream])
+
+    -- Ekg stuff
+    putStrLn "Starting ekg monitoring on port 8081"
+    ekgServer <- forkServer "localhost" 8081
+
+    -- TODO: this bit is kinda gross but it lets us kill the ekg server when the wai or any other children threads dies.
+    E.catch (race_
+            (do
+                putStrLn "Starting server on port 8080"
+                toWaiApp (application "src/stdlib.scm") >>= run 8080
+            )
+            (do
+                putStrLn "Starting nsq service"
+                conf <- NSQ.defaultConfig "66.175.216.197"
+
+                topicQueue <- newTQueueIO
+                replyQueue <- newTQueueIO
+
+                -- Connect
+                race_
+                    (NSQ.establish conf topicQueue replyQueue)
+                    (consumeMessages "src/stdlib.scm" topicQueue replyQueue)
+            )
+        ) (\(E.ErrorCall e) -> (do
+            putStrLn e
+            killThread $ serverThreadId $ ekgServer
+        ))
 
 -- TODO:
 --  - Limit size of incoming program 8KB maybe enough
