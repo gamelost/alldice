2cb7d8abc86e5685e24446320dd839d1d3fbdfdf
Anja Berens <pharaun666@gmail.com>
2015-06-14 20:08:08 -0700

    Integrated libsass with the project, and copy the html file over

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,79 @@
 module.exports = function(grunt) {
-	"use strict";
+    "use strict";
 
-	grunt.initConfig({
-		srcFiles: [
-			"psrc/**/*.purs"
-			, "bower_components/**/src/**/*.purs"
-			, "bower_components/**/src/**/*.purs.hs"
-		],
+    // http://stackoverflow.com/questions/23847250/grunt-with-grunt-sass-libsass-wrapper-slow-compile-time
 
-		dotPsci: ["<%=srcFiles%>"],
+    grunt.initConfig({
+        pursSrcFiles: [
+            "psrc/**/*.purs"
+            , "bower_components/**/src/**/*.purs"
+            , "bower_components/**/src/**/*.purs.hs"
+        ],
 
-		psc: {
-			options: {
-				main: "Example.Main",
-				modules: ["Example.Main"]
-			},
-			all: {
-				src: [ "<%=srcFiles%>" ],
-				dest: "dist/example.js"
-			}
-		},
-		watch: {
-			src: [ "<%=srcFiles%>" ],
-			tasks: ["psc:all"]
-		},
-	});
+        dotPsci: ["<%=pursSrcFiles%>"],
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks("grunt-purescript");
+        psc: {
+            options: {
+                main: "Example.Main",
+                modules: ["Example.Main"]
+            },
+            all: {
+                src: [ "<%=pursSrcFiles%>" ],
+                dest: "dist/example.js"
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: true,
+                sourceComments: false,
+                outputStyle: 'expanded'
+            },
+            all: {
+                files: {
+                    'dist/style.css': 'psrc/sass/style.scss'
+                }
+            }
+        },
 
-	grunt.registerTask("default", ["psc:all", "dotPsci"]);
+        copy: {
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [ 'psrc/**/*.html' ],
+                        dest: 'dist/',
+                        filter: 'isFile'
+                    }
+                ]
+            }
+        },
+
+        clean : {
+            src: [ 'dist' ]
+        },
+
+        watch: {
+            purs: {
+                files: [ "<%=pursSrcFiles%>" ],
+                tasks: ["psc:all"]
+            },
+            css: {
+                files: [ "psrc/**/*.scss" ],
+                tasks: ["sass:all"]
+            },
+            html: {
+                files: [ "psrc/**/*.html" ],
+                tasks: ["copy:all"]
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks("grunt-purescript");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.registerTask("default", ["psc:all", "dotPsci", "sass:all", "copy:all"]);
 };
